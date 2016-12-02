170320a856cb662f4a4a8c9ccb1995692147c1e2
Alberto.Cavadia <albertoc@blanclabs.com>
2016-12-02 16:49:36 -0400

    Added API request using fetch.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,7 +18,16 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
-})
+});
+
+app.get('/users', function (req, res) {
+  //Hard coding for simplicity. Pretend this hits a real daabase
+  res.json([
+    {"id": 1, "firstName": "Marcos", "lastName": "Correia", "email": "marcosc@blanclabs.com"},
+    {"id": 2, "firstName": "Milagros", "lastName": "Gómez", "email": "milagrosg@blanclabs.com"},
+    {"id": 3, "firstName": "Jesús", "lastName": "Gónzalez", "email": "jesusg@blanclabs.com"}
+  ]);
+});
 
 app.listen(port, function(err) {
   if (err) {
